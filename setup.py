4551f68ac7d3465946f68faed4ebf32f0600504f
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2023-11-16 18:36:14 +0000

    added amdrun to setup

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     author_email="hardware@huggingface.co",
     license="MIT",
     packages=find_namespace_packages(include=["optimum*"]),
+    entry_points={"console_scripts": ["amdrun=optimum.amd.cli:amdrun", "wrongrun=optimum.amd.cli:wrongrun"]},
     install_requires=INSTALL_REQUIRE,
     extras_require=EXTRAS_REQUIRE,
     include_package_data=True,
