3ab00b9093391057dd7b9dd979cb51b217bd89b9
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-12-15 11:39:49 +0100

    add accuracy test

--- tests/ryzenai/test_quantization.py
+++ tests/ryzenai/test_quantization.py
@@ -5,12 +5,15 @@ import tempfile
 import unittest
 from typing import Dict
 
+import evaluate
+import timm
 import torch
-from datasets import Dataset
+from datasets import Dataset, load_dataset
 from parameterized import parameterized
 from testing_utils import PYTORCH_TIMM_MODEL
+from tqdm import tqdm
 
-from optimum.amd.ryzenai import QuantizationConfig, RyzenAIOnnxQuantizer
+from optimum.amd.ryzenai import QuantizationConfig, RyzenAIModelForImageClassification, RyzenAIOnnxQuantizer
 from optimum.exporters.onnx import main_export
 from optimum.exporters.tasks import TasksManager
 from transformers import PretrainedConfig
@@ -82,6 +85,97 @@ class TestTimmQuantization(unittest.TestCase):
         dataset = dataset.with_format("torch")
 
         quantizer.quantize(quantization_config=quantization_config, dataset=dataset, save_dir=quantization_dir.name)
+
+        export_dir.cleanup()
+        quantization_dir.cleanup()
+
+    def test_quantization_quality(self):
+        dataset_name = "imagenet-1k"
+        model_name = "timm/res2next50.in1k"
+
+        timm_model = timm.create_model("res2next50.in1k", pretrained=True)
+        timm_model = timm_model.eval()
+
+        export_dir = tempfile.TemporaryDirectory()
+        quantization_dir = tempfile.TemporaryDirectory()
+
+        main_export(
+            model_name_or_path=model_name,
+            output=export_dir.name,
+            task="image-classification",
+        )
+
+        quantizer = RyzenAIOnnxQuantizer.from_pretrained(export_dir.name)
+
+        # NOTE: Vitis AI Quantizer 3.5 is broken with enable_dpu=True.
+        quantization_config = QuantizationConfig(enable_dpu=False)
+
+        calibration_set = load_dataset(dataset_name, split="train", streaming=True)
+        calibration_data = {"pixel_values": []}
+
+        data_config = timm.data.resolve_model_data_config(timm_model)
+        transforms = timm.data.create_transform(**data_config, is_training=False)
+
+        iterable_calibration_set = iter(calibration_set)
+        for i in range(100):
+            pil_image = next(iterable_calibration_set)["image"]
+            if pil_image.mode == "L":
+                # Three channels.
+                print("WARNING: converting greyscale to RGB")
+                pil_image = pil_image.convert("RGB")
+            pixel_values = transforms(pil_image)
+            calibration_data["pixel_values"].append(pixel_values)
+
+        calibration_dataset = Dataset.from_dict(calibration_data)
+        calibration_dataset = calibration_dataset.with_format("torch")
+
+        quantizer.quantize(
+            quantization_config=quantization_config, dataset=calibration_dataset, save_dir=quantization_dir.name
+        )
+
+        evaluation_set = load_dataset(dataset_name, split="validation", streaming=True)
+
+        # TODO: Evaluate on VitisAIExecutionProvider.
+        ryzen_model = RyzenAIModelForImageClassification.from_pretrained(
+            quantization_dir.name, provider="CPUExecutionProvider"
+        )
+
+        iterable_evaluation_set = iter(evaluation_set)
+
+        timm_evals = []
+        quantized_evals = []
+        reference_labels = []
+        for i in tqdm(range(300), desc="Inference..."):
+            data = next(iterable_evaluation_set)
+
+            reference_labels.append(data["label"])
+
+            pil_image = data["image"]
+
+            if pil_image.mode == "L":
+                # Three channels.
+                print("WARNING: converting greyscale to RGB")
+                pil_image = pil_image.convert("RGB")
+            pixel_values = transforms(pil_image).unsqueeze(0)
+
+            res = ryzen_model(pixel_values)
+            predicted_id = torch.argmax(res.logits, dim=-1).item()
+            quantized_evals.append(predicted_id)
+
+            res = timm_model(pixel_values)
+            predicted_id = torch.argmax(res, dim=-1).item()
+            timm_evals.append(predicted_id)
+
+        accuracy = evaluate.load("accuracy")
+
+        quantized_accuracy = accuracy.compute(references=reference_labels, predictions=quantized_evals)["accuracy"]
+        timm_accuracy = accuracy.compute(references=reference_labels, predictions=timm_evals)["accuracy"]
+
+        print("quantized_accuracy", quantized_accuracy)
+        print("timm_accuracy", timm_accuracy)
+
+        # TODO: Should get to 1% accuracy drop.
+        self.assertTrue((timm_accuracy - quantized_accuracy) / timm_accuracy < 0.05)
 
         export_dir.cleanup()
         quantization_dir.cleanup()
