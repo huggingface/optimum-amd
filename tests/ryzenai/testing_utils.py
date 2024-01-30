8afbff43797c5f27abf5e112689c64385b4d971d
Mohit Sharma <mohit21sharma.ms@gmail.com>
2024-01-30 13:52:49 +0530

    update the test list

--- tests/ryzenai/testing_utils.py
+++ tests/ryzenai/testing_utils.py
@@ -44,11 +44,13 @@ PYTORCH_TIMM_MODEL_SUBSET = {
 
 PYTORCH_TIMM_MODEL_FAST = {
     "default-timm-config": {
-        "timm/efficientnet_b0.ra_in1k": ["image-classification"],
-        "timm/mobilenetv2_050.lamb_in1k": ["image-classification"],
-        "timm/resnet18.a1_in1k": ["image-classification"],
-        "timm/vgg11.tv_in1k": ["image-classification"],
-        "timm/xception71.tf_in1k": ["image-classification"],
+        "timm/densenet121.ra_in1k": ["image-classification"],
+        "timm/ese_vovnet19b_dw.ra_in1k": ["image-classification"],
+        "timm/ghostnet_100.in1k": ["image-classification"],
+        "timm/inception_v4.tf_in1k": ["image-classification"],
+        "timm/repvgg_b0.rvgg_in1k": ["image-classification"],
+        "timm/resnet10t.c3_in1k": ["image-classification"],
+        "timm/vgg19.tv_in1k": ["image-classification"],
     }
 }
 
