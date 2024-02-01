c923c2393b6205f8fc14ddb85394cc33d903e8fd
fxmarty <9808326+fxmarty@users.noreply.github.com>
2024-02-01 11:30:55 +0100

    update data loading & perplexity

--- optimum/amd/brevitas/data_utils.py
+++ optimum/amd/brevitas/data_utils.py
@@ -12,33 +12,45 @@ from tqdm import tqdm
 
 
 @torch.no_grad()
-def model_eval_accelerate(model, data: List[Dict], seqlen: int):
-    encodings = {"input_ids": torch.empty((0,), dtype=torch.int64)}
+def model_eval_accelerate(model, data: List[Dict], context_length: int, tokenizer):    
+    model = model.eval()
+    device = data[0]["input_ids"].device
 
-    # TODO: It is bad practice to compute perplexity like that (mixing up sequences in a single long sequence). The way evaluate handles it is cleaner: https://github.com/huggingface/evaluate/blob/8dfe05784099fb9af55b8e77793205a3b7c86465/metrics/perplexity/perplexity.py#L140
-    for batch in data:
-        encodings["input_ids"] = torch.cat((encodings["input_ids"], batch["input_ids"].squeeze(0)), dim=0)
+    ppls = []
+    for sample in tqdm(data):
+        sample_length = sample["input_ids"].shape[1]
+        for start_index in range(0, sample_length, context_length * 2):
+            end_index = min(start_index + sample_length, sample_length - 1)
 
-    encodings["input_ids"] = encodings["input_ids"].unsqueeze(0)
+            subsample = {"input_ids": sample["input_ids"][:, start_index:end_index + 1], "attention_mask": sample["attention_mask"][:, start_index:end_index + 1]}
 
-    nsamples = encodings["input_ids"].numel() // seqlen
-    use_cache = model.config.use_cache
-    model.config.use_cache = False
-    with torch.no_grad():
-        nlls = []
-        for i in tqdm(range(nsamples)):
-            batch = encodings["input_ids"][:, (i * seqlen) : ((i + 1) * seqlen)].cuda()
-            attention_mask = torch.ones_like(batch)
-            lm_logits = model(input_ids=batch, attention_mask=attention_mask)["logits"]
-            shift_logits = lm_logits[:, :-1, :].contiguous()
-            shift_labels = (encodings["input_ids"][:, (i * seqlen) : ((i + 1) * seqlen)][:, 1:]).cuda()
-            loss_fct = nn.CrossEntropyLoss()
-            loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-            neg_log_likelihood = loss.float() * seqlen
-            nlls.append(neg_log_likelihood)
-        ppl = torch.exp(torch.stack(nlls).sum() / (nsamples * seqlen))
-    model.config.use_cache = use_cache
-    return ppl
+            # Add BOS token.
+            bos_tokens_tensor = torch.tensor([[tokenizer.bos_token_id]]).to(device)
+            subsample["input_ids"] = torch.cat([bos_tokens_tensor, subsample["input_ids"]], dim=1)
+            subsample["attention_mask"] = torch.cat(
+                [torch.ones(bos_tokens_tensor.size(), dtype=torch.int64).to(device), subsample["attention_mask"]], dim=1
+            )
+
+            with torch.no_grad():
+                lm_logits = model(**subsample)["logits"]
+
+            reference_labels = subsample["input_ids"][:, context_length:]
+
+            shift_logits = lm_logits[:, context_length - 1:-1]
+
+            # Fuse batch and sequence length dimensions.
+            reference_labels = reference_labels.view(reference_labels.shape[-1])
+            shift_logits = shift_logits.view(-1, shift_logits.shape[-1])
+
+            loss_fct = nn.CrossEntropyLoss(reduction="none")
+
+            loss = loss_fct(shift_logits, reference_labels)
+
+            ppls_subsample = torch.exp(loss.mean())
+
+            ppls += ppls_subsample.tolist()
+    
+    return np.mean(ppls)
 
 
 def get_wikitext2(tokenizer: Any, seqlen: int, nsamples: int, split: str = "train"):
@@ -46,17 +58,23 @@ def get_wikitext2(tokenizer: Any, seqlen: int, nsamples: int, split: str = "trai
         data = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
     elif split == "validation":
         data = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
-    # length of 288059 should be enough
-    text = "".join([" \n" if s == "" else s for s in data["text"][:1000]])
-
-    enc = tokenizer(text, return_tensors="pt")
+        
     dataset = []
-    for _ in range(nsamples):
-        i = random.randint(0, enc.input_ids.shape[1] - seqlen - 1)
-        j = i + seqlen
-        inp = enc.input_ids[:, i:j]
-        attention_mask = torch.ones_like(inp)
-        dataset.append({"input_ids": inp, "attention_mask": attention_mask})
+    with tqdm(total=nsamples) as pbar:
+        while len(dataset) < nsamples:
+            data_index = random.randint(0, len(data) - 1)
+
+            enc = tokenizer(data[data_index]["text"], return_tensors="pt")
+
+            if enc["input_ids"].shape[1] < seqlen:
+                continue
+
+            start_idx = random.randint(0, enc["input_ids"].shape[1] - seqlen)
+            end_idx = start_idx + seqlen - 1
+            attention_mask = torch.ones((1, seqlen), dtype=torch.int64)
+            dataset.append({"input_ids": enc["input_ids"][:, start_idx:end_idx + 1], "attention_mask": attention_mask})
+            pbar.update(1)
+    
     return dataset
 
 
@@ -70,19 +88,23 @@ def get_c4(tokenizer: Any, seqlen: int, nsamples: int, split: str = "train"):
             data_files={"validation": "en/c4-validation.00000-of-00008.json.gz"},
         )
     dataset = []
-    for _ in range(nsamples):
-        while True:
-            i = random.randint(0, len(data) - 1)
-            enc = tokenizer(data[i]["text"], return_tensors="pt")
-            if enc.input_ids.shape[1] >= seqlen:
-                break
-        i = random.randint(0, enc.input_ids.shape[1] - seqlen - 1)
-        j = i + seqlen
-        inp = enc.input_ids[:, i:j]
-        attention_mask = torch.ones_like(inp)
-        dataset.append({"input_ids": inp, "attention_mask": attention_mask})
+    with tqdm(total=nsamples) as pbar:
+        while len(dataset) < nsamples:
+            data_index = random.randint(0, len(data) - 1)
 
+            enc = tokenizer(data[data_index]["text"], return_tensors="pt")
+
+            if enc["input_ids"].shape[1] < seqlen:
+                continue
+
+            start_idx = random.randint(0, enc["input_ids"].shape[1] - seqlen)
+            end_idx = start_idx + seqlen - 1
+            attention_mask = torch.ones((1, seqlen), dtype=torch.int64)
+            dataset.append({"input_ids": enc["input_ids"][:, start_idx:end_idx + 1], "attention_mask": attention_mask})
+            pbar.update(1)
+    
     return dataset
+
 
 
 def get_dataset(
