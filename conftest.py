62c446ec7a9a340bbf7ce4478a511c129fc2fbdb
Mohit Sharma <mohit21sharma.ms@gmail.com>
2024-02-12 12:34:58 +0530

    style

--- conftest.py
+++ conftest.py
@@ -12,6 +12,5 @@ def pytest_terminal_summary(terminalreporter):
     from transformers.testing_utils import pytest_terminal_summary_main
 
     make_reports = terminalreporter.config.getoption("--make-reports")
-    # from pdb import set_trace; set_trace()
     if make_reports:
         pytest_terminal_summary_main(terminalreporter, id=make_reports)
