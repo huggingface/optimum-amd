241ddb34386e43fdf44fa96d5751e3ed74887d9d
fxmarty <9808326+fxmarty@users.noreply.github.com>
2024-02-16 11:14:16 +0100

    lazy import

--- optimum/amd/__init__.py
+++ optimum/amd/__init__.py
@@ -1,5 +1,29 @@
 # Copyright 2023 The HuggingFace Team. All rights reserved.
 # Licensed under the MIT License.
 
-from .brevitas.configuration import BrevitasQuantizationConfig
-from .brevitas.quantizer import BrevitasQuantizer
+from typing import TYPE_CHECKING
+
+from transformers.utils import _LazyModule
+
+
+_import_structure = {
+    "brevitas.configuration": [
+        "BrevitasQuantizationConfig",
+    ],
+    "brevitas.quantizer": [
+        "BrevitasQuantizer",
+    ],
+}
+
+if TYPE_CHECKING:
+    from .brevitas.configuration import BrevitasQuantizationConfig
+    from .brevitas.quantizer import BrevitasQuantizer
+else:
+    import sys
+
+    sys.modules[__name__] = _LazyModule(
+        __name__,
+        globals()["__file__"],
+        _import_structure,
+        module_spec=__spec__,
+    )
