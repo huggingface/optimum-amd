bb4f843b55a655f13c429e73d4c3fb0b3c1558f7
Giuseppe Franco <giuseppefranco4@gmail.com>
2024-03-14 14:54:31 +0000

    Add ONNX rewriter

--- optimum/amd/brevitas/export.py
+++ optimum/amd/brevitas/export.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from pathlib import Path
 from typing import Any, Callable, Dict, List, Optional, Union
 
@@ -49,3 +50,20 @@ def onnx_export_from_quantized_model(
             no_post_process=True,
             **kwargs_shapes,
         )
+=======
+ 
+import torch
+from brevitas.export.onnx.standard.qcdq.manager import StdQCDQONNXManager
+from brevitas_examples.llm.llm_quant.export import brevitas_proxy_export_mode 
+from optimum.exporters.onnx import onnx_export_from_model
+
+
+def export_quantized_model(quantized_model, path, task="text-generation-with-past"):
+    with torch.no_grad(), brevitas_proxy_export_mode(quantized_model, export_manager=StdQCDQONNXManager):
+        onnx_export_from_model(
+            quantized_model,
+            path,
+            task=task,
+            do_validation=False,
+            no_post_process=True)
+>>>>>>> Feat: export dq only
