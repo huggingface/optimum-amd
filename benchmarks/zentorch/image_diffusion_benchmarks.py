34e5f61a1ae47c4a96d0c71e096f74090783a370
IlyasMoutawwakil <ilyas.moutawwakil@gmail.com>
2024-04-08 08:12:16 +0000

    added image diffusion benchmarks

---
+++ benchmarks/zentorch/image_diffusion_benchmarks.py
@@ -0,0 +1,69 @@
+from optimum_benchmark.backends.pytorch.config import PyTorchConfig
+from optimum_benchmark.benchmarks.inference.config import InferenceConfig
+from optimum_benchmark.experiment import ExperimentConfig, launch
+from optimum_benchmark.launchers.process.config import ProcessConfig
+from optimum_benchmark.logging_utils import setup_logging
+
+
+setup_logging(level="INFO")
+
+REPO_ID = "optimum-amd/ci-benchmarks"
+EXPERIMENT_NAME = "image_diffusion"
+
+IMAGE_DIFFUSION_PIPELINES_LIST = [
+    "stabilityai/stable-diffusion-2-1",
+]
+INPUT_SHAPES = {
+    "batch_size": 1,
+}
+CALL_KWARGS = {
+    "num_inference_steps": 2,
+    "num_images_per_prompt": 1,
+}
+TORCH_COMPILE_CONFIG = {
+    "backend": "zentorch",
+}
+
+
+def benchmark_image_diffusion():
+    for model in IMAGE_DIFFUSION_PIPELINES_LIST:
+        launcher_config = ProcessConfig(start_method="spawn")  # isolated process
+        benchmark_config = InferenceConfig(
+            memory=True,
+            latency=True,
+            input_shapes=INPUT_SHAPES,
+            call_kwargs=CALL_KWARGS,
+        )
+        backend_config = PyTorchConfig(
+            model=model,
+            device="cpu",
+            no_weights=True,
+            torch_compile=True,
+            torch_compile_config=TORCH_COMPILE_CONFIG,
+        )
+
+        experiment_config = ExperimentConfig(
+            experiment_name=EXPERIMENT_NAME,
+            benchmark=benchmark_config,
+            launcher=launcher_config,
+            backend=backend_config,
+        )
+
+        benchmark_report = launch(experiment_config)
+
+        experiment_config.push_to_hub(
+            save_path=f"zentorch/{EXPERIMENT_NAME}/{model}",
+            commit_message="Added experiment config",
+            repo_id=REPO_ID,
+            private=True,
+        )
+        benchmark_report.push_to_hub(
+            save_path=f"zentorch/{EXPERIMENT_NAME}/{model}",
+            commit_message="Added benchmark report",
+            repo_id=REPO_ID,
+            private=True,
+        )
+
+
+if __name__ == "__main__":
+    benchmark_image_diffusion()
